eaf73bd3137aa264759a55ff41c6636f7f06fbf1
Vir1oN <xsv7yy@gmail.com>
2021-03-12 19:50:39 +0200

    products endpoint fix

--- dz_40/routers/main.router.js
+++ dz_40/routers/main.router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRouter = require('./user.router');
 const authRouter = require('./auth.router');
+const productsRouter = require('./products.router');
 
 router.use('/users', userRouter);
 router.use('/login', authRouter);
+router.use('/products', productsRouter);
 
 module.exports = router;
